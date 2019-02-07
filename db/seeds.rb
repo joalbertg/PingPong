38abc4bd2487256f1bb98f3d56b16aaa74d6dd00
Joalbert <joalbertgonzalez@gmail.com>
2019-02-07 16:32:42 -0600

    added: users with faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |index|
+    User.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      password: '123456'
+    )
+    p "#{index + 1} user created"
+end
