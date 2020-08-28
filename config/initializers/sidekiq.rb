1fb3400ad3fca24715c427a8154a3ff1c8a0e8eb
Joalbert <joalbertgonzalez@gmail.com>
2020-08-28 17:59:09 -0500

    [ADD]🚀 docker files added

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,1 +1,16 @@
 Rails.application.config.active_job.queue_adapter = :sidekiq
+
+Sidekiq.configure_server do |config|
+  config.redis = {
+    host: ENV['REDIS_HOST'],
+    port: ENV['REDIS_PORT'] || '6379'
+  }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {
+    host: ENV['REDIS_HOST'],
+    port: ENV['REDIS_PORT'] || '6379'
+  }
+end
+
